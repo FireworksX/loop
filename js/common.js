01ab6bc5767b9c1a7867e07f7819bdf415b8aeb0
FireworksX <abeltinsh.a@icloud.com>
2016-10-18 20:50:22 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider-wrap').slick({
+        dots: true
+    });
+	
+	$('.customers-slider').slick({
+		arrows: false,
+		autoplay: true,
+  		autoplaySpeed: 4000,
+	});
+});
+
+
